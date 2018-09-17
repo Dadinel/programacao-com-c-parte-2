50c4a79c6ffa03d3edc2fe94d37a246fe96127cf
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-09-17 13:16:03 -0300

    Aula 02, atividade 04

--- forca.c
+++ forca.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char letra1 = 'M';
-    char letra2 = 'E';
-    char letra3 = 'L';
-    char letra4 = 'A';
-    char letra5 = 'N';
-    char letra6 = 'C';
-    char letra7 = 'I';
-    char letra8 = 'A';
+    char palavrasecreta[20];
 
-    printf("%c%c%c%c%c%c%c%c", letra1, letra2, letra3, letra4, letra5, letra6, letra7, letra8);
+    //sprintf(palavrasecreta, "MELANCIA");
 
-    int notas[10];
+    int acertou = 0;
+    int enforcou = 1;
 
-    notas[0] = 10;
-    notas[2] = 9;
-    notas[3] = 8;
-    notas[9] = 4;
+    printf("%s", palavrasecreta);
 
-    printf("%d %d %d", notas[0], notas[2], notas[9]);
+    do {
+        char chute;
+        scanf("%c", &chute);
+
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            printf("Estou vendo a letra %c na posicao %d\n", palavrasecreta[i], i);
+            if(palavrasecreta[i] == chute) {
+                printf("A posicao %d tem essa letra!\n", i);
+            }
+        }
+    } while(!acertou && !enforcou);
+
+    printf("");
 }
