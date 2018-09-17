607d1615457956720582f74bffc07d28ec856678
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-09-17 12:59:11 -0300

    Teste com arrays.

--- forca.c
+++ forca.c
@@ -11,4 +11,13 @@ int main() {
     char letra8 = 'A';
 
     printf("%c%c%c%c%c%c%c%c", letra1, letra2, letra3, letra4, letra5, letra6, letra7, letra8);
+
+    int notas[10];
+
+    notas[0] = 10;
+    notas[2] = 9;
+    notas[3] = 8;
+    notas[9] = 4;
+
+    printf("%d %d %d", notas[0], notas[2], notas[9]);
 }
